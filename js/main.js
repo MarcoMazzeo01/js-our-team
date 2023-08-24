bfe726470af5a19e78f19e93ec44f1f899949a62
plastikSun <marcom2k01@gmail.com>
2023-08-24 14:37:38 +0200

    complete: milestone 0

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const team = {
+    member1 : {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        img: "./img/wayne-barnett-founder-ceo.jpg"
+    },
+
+    member2 : {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        img: "./img/angela-caroll-chief-editor.jpg"
+    },
+
+    member3 : {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        img: "./img/walter-gordon-office-manager.jpg"
+    },
+
+    member4 : {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        img: "./img/angela-lopez-social-media-manager.jpg"
+    },
+
+    member5 : {
+        name: "Scott Estrada",
+        role: "Developer",
+        img: "./img/scott-estrada-developer.jpg"
+    },
+
+    member6 : {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        img: "./img/barbara-ramos-graphic-designer.jpg"
+    },
+
+
+}
+
+console.log(team)
